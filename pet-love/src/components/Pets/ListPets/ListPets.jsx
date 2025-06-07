9da3f0e83b5a082ac4841a06b5d1868cef473bf3
ErickWarmling <warmling.erick@gmail.com>
2025-06-07 14:20:11 -0300

    replicacao de forms

--- pet-love/src/components/Pets/ListPets/ListPets.jsx
+++ pet-love/src/components/Pets/ListPets/ListPets.jsx
@@ -3,6 +3,7 @@ import AddButton from '../../Grid/AddButton/AddButton';
 import GridContent from '../../Grid/GridContent/GridContent';
 import FilterDropdown from '../../Grid/FilterDropdown/FilterDropdown';
 import { listPets } from '../../../api/pets';
+import ModalForm from '../../Form/ModalForm';
 
 const columns = [
     { header: 'ID', accessor: 'id' },
@@ -15,6 +16,7 @@ const columns = [
 
 function ListPets() {
     const [filteredData, setFilteredData] = useState([]);
+    const [showModal, setShowModal] = useState(false);
 
     useEffect(() => {
         async function fetchData() {
@@ -56,6 +58,27 @@ function ListPets() {
         setFilteredData(filtered);
     };
 
+    const handleAdd = (newPet) => {
+        const newId = data.length ? Math.max(...data.map(d => d.id)) +1 : 1;
+        setData(prev => [...prev, { id: newId, ... newPet }]);
+    };
+
+    const formFields = [
+        { name: 'name', label: 'Nome' },
+        { name: 'owner', label: 'Dono', type: 'select', options: [
+            'Lucas Silva',
+            'Ana Costa',
+            'João Pereira',
+            'Mariana Rocha',
+            'Carlos Almeida'
+        ]},
+        { name: 'type', label: 'Tipo', type: 'select', options: ['Cachorro', 'Gato'] },
+        { name: 'race', label: 'Raça' },
+        { name: 'date_birth', label: 'Data de Nascimento', type: 'date' },
+        { name: 'observation', label: 'Observações', type: 'textarea', rows: 4 },
+        { name: 'image', label: 'Foto', type: 'file'},
+    ];
+
     return (
         <section className="ListPets py-4">
             <div className="container">
@@ -65,7 +88,7 @@ function ListPets() {
                         <h2 className="m-0">PETS</h2>
                     </div>
                     <div className="col-auto">
-                        <AddButton text="Novo Pet" onClick={() => console.log('NOVO PET')} />
+                        <AddButton text="Novo Pet" onClick={() => setShowModal(true)} />
                     </div>
                 </div>
 
@@ -82,6 +105,15 @@ function ListPets() {
                         <GridContent data={filteredData} columns={columns} />
                     </div>
                 </div>
+
+                {/* Modal Form */}
+                <ModalForm
+                    show={showModal}
+                    onClose={() => setShowModal(false)}
+                    title="Novo Pet"
+                    fields={formFields}
+                    onSubmit={handleAdd}
+                />
             </div>
         </section>
     );
