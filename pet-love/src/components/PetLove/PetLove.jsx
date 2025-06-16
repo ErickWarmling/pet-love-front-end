929e05dfed9ca74c0f262dc69756e100e30b3db0
ErickWarmling <warmling.erick@gmail.com>
2025-06-16 15:23:15 -0300

    Inserimento da rota de Usuários

--- pet-love/src/components/PetLove/PetLove.jsx
+++ pet-love/src/components/PetLove/PetLove.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import ListDonos from "../Donos/ListDonos/ListDonos";
 import TopMenu from "../TopMenu/TopMenu";
 import ListSolicitacaoAdocao from "../Adocoes/ListSolicitacaoAdocao/ListSolicitacaoAdocao";
@@ -6,24 +6,37 @@ import ListConsultas from "../Consultas/ListConsultas/ListConsultas";
 import ListPets from "../Pets/ListPets/ListPets";
 import ListFuncionarios from "../Funcionarios/ListFuncionarios/ListFuncionarios";
 import Login from "../Login/Login";
+import ListUsuarios from "../Usuarios/ListUsuarios";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
-export default function PetLove() {
+function App() {
+    const location = useLocation();
+    const hideMenu = location.pathname === "/login";
+
     return (
         <div>
             <ToastContainer position="top-right" autoClose={3000} />
-            <TopMenu></TopMenu>
+            {!hideMenu && <TopMenu />}
             <BrowserRouter>
                 <Routes>
                     <Route path="/funcionarios" element={<ListFuncionarios/>}></Route>
                     <Route path="/donos" element={<ListDonos/>}></Route>
                     <Route path="/pets" element={<ListPets/>}></Route>
                     <Route path="/consultas" element={<ListConsultas/>}></Route>
+                    <Route path="/usuarios" element={<ListUsuarios/>}></Route>
                     <Route path="/adocoes" element={<ListSolicitacaoAdocao/>}></Route>
                     <Route path="/login" element={<Login />}></Route>
                 </Routes>
             </BrowserRouter>
         </div>
     )
+}
+
+export default function PetLove() {
+    return (
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    );
 }
