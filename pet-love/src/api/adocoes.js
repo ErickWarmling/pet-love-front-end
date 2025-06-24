36a3eb612d090a51a275d129fb58396387122a7f
Lucas E. Gitirana <gitiranalucas5@gmail.com>
2025-06-23 22:54:33 -0300

    crud solicitacoes

--- pet-love/src/api/adocoes.js
+++ pet-love/src/api/adocoes.js
@@ -1,3 +1,6 @@
 import { petLoveApi } from "./api";
 
 export const listAdocoes = () => petLoveApi.get(`/solicitacao-adocao`)
+export const createAdocao = (data) => petLoveApi.post('/solicitacao-adocao', data)
+export const updateAdocao = (id, data) => petLoveApi.put(`/solicitacao-adocao/${id}`, data)
+export const deleteAdocao = (id) => petLoveApi.delete(`/solicitacao-adocao/${id}`)
--- pet-love/src/api/pets.js
+++ pet-love/src/api/pets.js
@@ -1,6 +1,7 @@
 import { petLoveApi } from "./api";
 
 export const listPets = () => petLoveApi.get(`/pets`)
+export const listPetsSemDono = (id) => petLoveApi.get(`/pets/sem-dono`)
 export const createPet = (data) => petLoveApi.post(`/pets`, data)
 export const updatePet = (idPet, data) => petLoveApi.put(`/pets/${idPet}`, data)
 export const deletePet = (idPet) => petLoveApi.delete(`/pets/${idPet}`)
